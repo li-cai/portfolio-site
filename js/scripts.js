ea937befdcde9b205bbc55fa8829a11a120a2d26
Cailin <cxl2467@rit.edu>
2014-01-16 16:38:58 -0500

    Update animate functions for banner and nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,42 @@
 $(document).ready(function() {
 
-    // navigation link hover effect
+    // navigation link effects
     $('div.nav-flag').each(function() {
         var bgColor = $(this).css('background-color');
 
+        // hover effect
         $(this).hover(
             function() {
-                $(this).stop().animate({backgroundColor: '#FFA476'}, 200);
-                $(this).children('span:first').stop().animate({color: '#FFA476'}, 200);
+                $(this).stop().animate({backgroundColor: '#e7eef9'}, 200);
+                $(this).children('span:first').stop().animate({color: '#e7eef9'}, 200);
             },
             function() {
                 $(this).stop().animate({backgroundColor: bgColor}, 200);
                 $(this).children('span:first').stop().animate({color: bgColor}, 200);
             }
         ); // end hover
-    }); // end each
 
-    // 
+        // click effect
+        $(this).click(function() {
+            var bottomHeight = $(window).height() - 162;
+            $('#nav').animate({bottom: bottomHeight},
+                              {duration: 1800, queue: false,
+                              complete: function() {
+                                  $('#nav').attr('id', 'top-nav');
+                              }}
+                             ); // end nav animate
+
+
+            $('#banner').animate({height: '120px'}, 
+                                 {duration: 1800, 
+                                  queue: false, 
+                                  complete: function () {
+                                      $('#about').hide();
+                                      $('#banner').attr('id', 'top-banner'); 
+                                  }}
+                                ); // end banner animate
+        }); // end click
+
+    }); // end each
 
 }); // end ready
