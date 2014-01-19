226bcec53cf7e57f5ea097f1789526a9d6d4bcbb
Cailin <cxl2467@rit.edu>
2014-01-19 17:53:43 -0500

    Update content display when links are clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,34 @@
 $(document).ready(function() {
-    $('div.content').css({'position': 'relative', 'top': $(window).height(), 'display': 'none'});
-    $('#contact').css('visibility', 'hidden');
+    $('div.content').css({'position': 'relative', 'top': $(window).height()});
+
+    $('#projects').css('display', 'none');
+    $('#resume').css('display', 'none');
+
+    $('#projects-link').click(function() {
+        $('#projects').attr('style', '');
+        $('#resume').css('display', 'none');
+    });
+
+    $('#resume-link').click(function() {
+        $('#resume').attr('style', '');
+        $('#projects').css('display', 'none');
+    });
 
     // navigation link effects
     $('div.nav-flag').each(function() {
-        var bgColor = $(this).css('background-color');
-
         // hover effect
         $(this).hover(
             function() {
-                $(this).stop().animate({backgroundColor: '#e7eef9'}, 200);
-                $(this).children('span:first').stop().animate({color: '#e7eef9'}, 200);
+                $(this).stop().animate({opacity: '0.65'}, 200);
             },
             function() {
-                $(this).stop().animate({backgroundColor: bgColor}, 200);
-                $(this).children('span:first').stop().animate({color: bgColor}, 200);
+                $(this).stop().animate({opacity: '1.0'}, 200);
+                $(this).attr('style', '');
             }
         ); // end hover
 
         // click effect
         $(this).click(function() {
-            $('#contact').attr('style', '');
-
             $('.content').css('display', '');
             $('.content').animate({top: '162px'}, 
                                   {duration: 1400, queue: false,
@@ -31,7 +38,7 @@ $(document).ready(function() {
                                  ); // end content animate
 
             var bottomHeight = $(window).height() - 162;
-            $('#nav').css('margin-left', '0');
+            $('#nav').css({'margin-left': '0', 'width': '40.36%'});
             $('#nav').animate({bottom: bottomHeight},
                               {duration: 1400, queue: false,
                               complete: function() {
